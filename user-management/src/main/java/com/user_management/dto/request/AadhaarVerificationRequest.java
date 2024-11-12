48b8a1431d43a755071efb14ed2b4eb4123fb185
chopan-m <chopanm093@gmail.com>
2024-11-12 21:43:40 +0530

    ✨ feat: added adhar based verfication api,s

---
+++ user-management/src/main/java/com/user_management/dto/request/AadhaarVerificationRequest.java
@@ -0,0 +1,14 @@
+package com.user_management.dto.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AadhaarVerificationRequest {
+    private String aadhaarNumber;
+    private String mobileNumber;
+    private Long userId;
+} 
